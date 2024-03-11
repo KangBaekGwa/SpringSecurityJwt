deff9f734ea7ea28255572e7c9d3141fd8c1195e
KangBaekGwa <ksu9801@gmail.com>
2024-03-12 00:28:12 +0900

    feat : first commit  - setting

---
+++ src/main/java/baekgwa/SpringJWT/controller/JoinController.java
@@ -0,0 +1,26 @@
+package baekgwa.SpringJWT.controller;
+
+import baekgwa.SpringJWT.dto.JoinDTO;
+import baekgwa.SpringJWT.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+        joinService.joinProcess(joinDTO);
+        return "ok";
+    }
+}
